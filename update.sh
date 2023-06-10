1543c4f659e65cb94a6d66463924900f77fc5ae5
Simone Torino <simone.torino.12@gmail.com>
2023-06-10 12:14:14 +0200

    s

--- update.sh
+++ update.sh
@@ -2,7 +2,6 @@
 
 # Aggiorna codice raspUI dopo aver fatto push dal pc
 
-sudo -s
 cd /home/pi/Project_PLCS_raspUI
 git pull https://github.com/simone-torino/Project_PLCS_raspUI master
 sudo systemctl restart flask
