8167e88d34f739a56e3f53e60dee962d4f724f26
Simone Torino <simone.torino.12@gmail.com>
2023-06-10 12:12:35 +0200

    up

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@
 
 sudo -s
 cd /home/pi/Project_PLCS_raspUI
-git pull
+git pull https://github.com/simone-torino/Project_PLCS_raspUI master
 sudo systemctl restart flask
 echo "Code updated and ready to be tested!"
