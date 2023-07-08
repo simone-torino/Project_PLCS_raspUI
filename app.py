ea151ff7698b13b6ec0b9694b68e077849f4659a
Simone Torino <simone.torino.12@gmail.com>
2023-07-08 17:20:00 +0200

    fix

--- app.py
+++ app.py
@@ -151,8 +151,8 @@ def read():
 
                     flag_timeOut = 0 # 1 c'è errore, 0 tutto apposto
 
-                    for x in range (0, len(time_IN)):
-                        if (not ((time_IN[x] is None) and (time_OUT[x] is None))) : #se tra tutti i timestamp trovati ce n'è uno che ha time_IN == None allora significa che voglio uscire prima di entrare
+                    #se i vettori hanno la stessa lunghezza significa che non sono entrato
+                    if (len(time_IN)) == (len(time_OUT)) : 
                             flag_timeOut = 1
 
                     if flag_timeOut == 0: #ok --> sto uscendo dopo essere entrato
